c46cd69514f35c866df2169cabd599e13f93741f
Hobbes4 <kylefertile@gmail.com>
2016-05-18 17:42:50 -0700

    Added time and date selecting to create event Added favorite and background options to create event Made a back button for the menu for create event

--- app/src/main/java/com/industries/fertile/countdowner/NewDate.java
+++ app/src/main/java/com/industries/fertile/countdowner/NewDate.java
@@ -1,13 +1,34 @@
 package com.industries.fertile.countdowner;
 
+
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.format.DateFormat;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.DatePicker;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class NewDate extends AppCompatActivity {
+
+    TextView dateTextView;
+    TextView timeTextView;
+    Switch timeSwitch;
+    RelativeLayout timeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +36,111 @@ public class NewDate extends AppCompatActivity {
         setContentView(R.layout.activity_new_date);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        dateTextView = (TextView) findViewById(R.id.dateTextView);
+        timeTextView = (TextView) findViewById(R.id.timeTextView);
+        timeSwitch = (Switch) findViewById(R.id.switch1);
+        timeLayout = (RelativeLayout) findViewById(R.id.timeRLayout);
+        timeLayout.setVisibility(View.GONE);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        timeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    timeLayout.setVisibility(View.VISIBLE);
+                } else {
+                    timeLayout.setVisibility(View.GONE);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_newdate, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        switch (item.getItemId()) {
+
+            case R.id.action_new:
+                // Put in DB if all info is in there
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
 
 
     }
 
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            NewDate activity = (NewDate) getActivity();
+            TextView dateText = activity.dateTextView;
+            dateText.setText(month + " " + day + ", " + year);
+        }
+
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+            NewDate activity = (NewDate) getActivity();
+            TextView dateText = activity.timeTextView;
+            dateText.setText(hourOfDay + ":" + minute);
+        }
+    }
+
 }
+
+
